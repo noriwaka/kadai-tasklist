a3f939fa564927959dc006802c24e2d38cec8929
noriwaka <airsoft.manatsu@gmail.com>
2024-01-16 13:36:35 +0000

    complete

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // タスクリストを取得
+        $tasks = Task::all();
+        
+        //タスクリストをビューで表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $task = new Task;
+        
+        //タスク作成用のビューを表示
+        return view('tasks.create', [
+            'tasks' => $task,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        //保存後にトップページへリダイレクト
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // id(引数)の値で検索して取得
+        $task = Task::findOrFail($id);
+        
+        // ビューで表示
+        return view('tasks.show', [
+            'task' => $task,
+            ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // id(引数)の値で検索して取得
+        $task = Task::findOrFail($id);
+        
+        // ビューで表示
+        return view('tasks.edit', [
+            'task' => $task,
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        
+        // タスク更新するために、POSTデータを代入して更新
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        
+        $task->delete();
+        
+        return redirect('/');
+    }
+}
